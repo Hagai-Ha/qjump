d3b3e33119d67021bdfd3d343ce22382aa5b8bcf
Eliya Shlomo <eliyashlomo@h-MacBook-Pro-sl-Eliya.local>
2026-07-09 16:11:35 +0300

    homepage css

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,9 +3,12 @@ import "./HomePage.css";
 
 function Homepage() {
     
+
   return (
 
+   // homepage main div
     <div className="homepage">
+        {/* top navbar */}
       <nav className="navbar">
         <div className="logo">Qjump</div>
         <div className="nav-links">
@@ -15,7 +18,7 @@ function Homepage() {
           <span>Support</span>
         </div>
       </nav>
-
+        {/* hero scripts section*/}
       <header className="hero">
         <h1 className="hero__title">
           Qjump: Get Treated Sooner, <br /> 
@@ -26,7 +29,7 @@ function Homepage() {
           confidence, ensuring you and your loved ones get timely care.
         </p>
       </header>
-
+        {/* login  section*/}
       <section className="login-section">
         <h3>Log In</h3>
         <input 
@@ -42,7 +45,7 @@ function Homepage() {
         <button className="login-section__button">Log In with HMO</button>
         <p className="login-section__hint">Fast & Secure Access via your HMO credentials</p>
       </section>
-
+        {/* feature  section*/}
       <div className="features-container">
         <div className="feature-card">
           <div className="icon-box">📅</div>
@@ -60,7 +63,7 @@ function Homepage() {
           </div>
         </div>
       </div>
-
+        {/* footer section*/}
       <footer className="footer-legal">
         Legal Information © 2022-2022 - Tenvaos Blods, Inc. All rights reserved.
       </footer>
