89053b012c7f0b1dc5940732573657647e0638ff
rotemgrossman1 <rotem.grossman@live.biu.ac.il>
2026-07-09 14:33:53 +0300

    started working on the Appointment page, finished a basic non functional non component page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import { useState } from 'react'
 import './App.css'
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import PatientPage from './pages/PatientPage'
+// import HomePage from './pages/HomePage'
 function App() {
 
   return (
-    <>
-    </>
+    <Router>
+      <div className="App">
+        <Routes>
+          {/* <Route path="/" element={<HomePage />} /> */}
+          <Route path="/" element={<PatientPage />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
+
 
 export default App
