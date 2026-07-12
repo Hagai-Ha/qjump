b7f5e70f346c7a10c4e9687bf3501f058db6c8cf
Eliya Shlomo <eliyashlomo@h-MacBook-Pro-sl-Eliya.local>
2026-07-12 10:33:03 +0300

    added login error handling via red message

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -8,31 +8,26 @@ function LoginForm() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
   const navigate = useNavigate(); 
 
 
   const handleLogin = async (e) => {
     e.preventDefault();
     setLoading(true);
+    setErrorMessage(""); 
 
     const { error: authError } = await supabase.auth.signInWithPassword({
         email: email,
         password: password,
-      });
+    });
       
-      if (authError) {
-        console.error("Auth Error:", authError.message); 
-        alert("Error Login : " + authError.message);
+    if (authError) {
+        setErrorMessage("Incorrect Username or Password"); 
         setLoading(false);
         return;
-      }
-
-    if (authError) {
-        // failed - User Name or Password is worng
-      alert("User Name or Password is worng");
-      setLoading(false);
-      return;
     }
+
     const { data: userData, error: userError } = await supabase
       .from('Users')
       .select('*')
@@ -40,18 +35,21 @@ function LoginForm() {
       .single();
 
     if (userError || !userData) {
-      alert("User not found in the patient system");
+      setErrorMessage("User not found on system");
+      setLoading(false);
     } else {
         const patientId = userData.patientId; 
         navigate(`/patient/${patientId}`);
     }
     
     setLoading(false);
-  };
+};
+
 
 return (
     <form className="login-section" onSubmit={handleLogin}>
       <h3>Log In</h3>
+      {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
       <input 
         type="email" 
         placeholder="Email" 
