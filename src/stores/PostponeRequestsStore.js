67662b043b81721be9652463ff0dd6890ed1b86a
Hagai <hagaihamami@gmail.com>
2026-07-13 17:11:49 +0300

    elaborate console log request deletion

--- src/stores/PostponeRequestsStore.js
+++ src/stores/PostponeRequestsStore.js
@@ -48,7 +48,7 @@ class PostponeRequestsStore {
                 this.requests = this.requests.filter(req => req.request_id !== request_id);
             });
             
-            console.log(`Successfully deleted request #${request_id}`);
+            console.log(`Successfully deleted postpone request #${request_id}`);
         } catch (err) {
             console.error("Error deleting postpone request:", err);
         }
