5557d1971bc67d5662d7490a766990d128fa6705
rotemgrossman1 <rotem.grossman@live.biu.ac.il>
2026-07-13 12:09:56 +0300

    implement protected routes for patient and requests pages; enhance AuthStore to initialize user authentication state

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Support from "./pages/Support";
 import NotFound from "./pages/NotFound";
 import '@mantine/core/styles.css'; // Import Mantine styles
 import { MantineProvider } from '@mantine/core';
+import ProtectedRoute from './components/ProtectedRoute';
 function App() {
 
   return (
@@ -17,13 +18,23 @@ function App() {
         <div className="App">
           <Routes>
             <Route path="/" element={<HomePage />} />
-            <Route path="/patient/:patientId" element={<PatientPage />} />
-            <Route path="/patient/:patientId/requests" element={<RequestsPage />} />
             <Route path="/how-it-works" element={<HowItWorks />} />
             <Route path="/clinics" element={<Clinics />} />
             <Route path="/resources" element={<Resources />} />
             <Route path="/support" element={<Support />} />
             <Route path="*" element={<NotFound />} />
+            <Route path="/patient/:patientId" 
+              element={
+              <ProtectedRoute>
+                <PatientPage />
+              </ProtectedRoute>
+              } />
+            <Route path="/patient/:patientId/requests" 
+              element={
+              <ProtectedRoute>
+                <RequestsPage />
+              </ProtectedRoute>
+              } />
           </Routes>
         </div>
       </Router>
